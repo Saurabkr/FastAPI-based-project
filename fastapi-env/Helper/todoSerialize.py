78b974c0adbbfaf14aac4f314a5b7fa1fc2e80cb
Saurab Kumar <82012354+Saurabkr@users.noreply.github.com>
2025-03-29 13:55:34 +0530

    Delete fastapi-env directory

--- fastapi-env/Helper/todoSerialize.py
+++
@@ -1,7 +0,0 @@
-# def todoSerialize(book):
-#     return{
-#         "id" : str(book["_id"]),
-#         "title": book["title"],
-#         "description": book["description"],
-#         "is_completed": book["is_completed"]
-#     }
